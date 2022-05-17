febbdc9a90b030bfa9c5f8ba70b7e5dbb6b39c6e
AndresXW5 <73656983+AndresXW5@users.noreply.github.com>
2022-05-17 11:30:57 +0200

    1.4.5
    Cambios en variables importantes

--- IronBugWeb/src/app/app.component.spec.ts
+++ IronBugWeb/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GamifiK'`, () => {
+  it(`should have as title 'ironBug'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GamifiK');
+    expect(app.title).toEqual('ironBug');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GamifiK app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ironBug app is running!');
   });
 });
